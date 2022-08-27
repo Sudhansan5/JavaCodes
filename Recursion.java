0030203a5b081ac90e168f99cf2764782a328985
Sudhanshu Kumar <sudhansan5@gmail.com>
2022-08-27 11:04:45 +0530

    check palindrome

--- Recursion.java
+++ Recursion.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Recursion {
-   void printOneToN(int i,int n){
+    int arr[]={1,2,3,4,5};
+    String name;
+
+    ArrayList<Integer> arrayList= new ArrayList<Integer>(Arrays.asList(1,2,3));
+    int n = arrayList.size();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    void printOneToN(int i, int n){
         if(i==n){
             System.out.println(i);
             return;
@@ -24,12 +37,61 @@ public class Recursion {
         }
         return n+sumOfN(n-1);
     }
+
+    void reverseArray(int l,int r){
+        if(l>r){
+            return;
+        }
+        int temp;
+        temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        reverseArray(l+1,r-1);
+    }
+
+    boolean palindrome(int l,int r){
+       if(l>=r){
+           return true;
+       }
+       if(name.charAt(l)!=name.charAt(r)){
+           return false;
+       }
+       return palindrome(l+1,r-1);
+
+    }
+
+    void printSubsequence(int i, ArrayList<Integer> arrList){
+        if(i==n){
+            System.out.println(arrList);
+            return;
+        }
+
+        arrList.add(arrayList.get(i));
+        printSubsequence(i+1,arrList);
+        arrList.remove(arrList.size()-1);
+        printSubsequence(i+1,arrList);
+
+    }
+
+
     public static void main(String[] args) {
        Recursion r= new Recursion();
 //        r.printOneToN(1,5);
         System.out.println();
 //        r.printNToOne(10);
-        int sum=r.sumOfN(5);
-        System.out.println(sum);
+//        int sum=r.sumOfN(5);
+//        System.out.println(sum);
+
+
+//        r.reverseArray(0,r.arr.length-1);  // reverse array
+//        for (int val:r.arr) {
+//            System.out.println(val);
+//        }
+//        String name="madam";     // palindrome
+//        r.setName(name);
+//        System.out.println( r.palindrome(0,name.length()-1));
+//        System.out.println();
+        ArrayList<Integer> arrList= new ArrayList<>();
+          r.printSubsequence(0,arrList);
     }
 }
--- Test.java
+++ Test.java
@@ -1,30 +1,9 @@
-import java.util.Arrays;
-import java.util.HashMap;
 
-class Test{
+class Test {
 
-    public static void main(String args[]){
-    int arr[][]={{1,2,3,4,5},{2,4,6,7,8},{2,3,4,5,6},{3,6,7,8,9},{2,6,5,4,3}};
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    int temp;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr[i].length; j++) {
-                temp=arr[i][j];
-                arr[i][j]=arr[j][i];
-                arr[j][i]=temp;
-            }
-        }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
+    public static void main(String[] args) {
+// longest palindrome string
+
     }
+
 }
